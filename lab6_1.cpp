32898ac3a3b27f1c20b0e8416c93bddde303b2ae
Winney <tatakornaow@gmail.com>
2022-12-09 16:19:14 +0700

    Lab6.2 completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,26 @@
 #include<iostream>
 using namespace std;
 
+int number;
+int oddNumber;
+int evenNumber;
+
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    while (true)
+    {
+        cout << "Enter an integer: ";
+        cin >> number;
+        if (number==0){
+            break;
+        }else if(number%2 == 0){
+            evenNumber ++;
+        }else{
+            oddNumber ++;
+        }
+    }
+    
+
+    cout << "#Even numbers = " << evenNumber << "\n";
+    cout << "#Odd numbers = "<< oddNumber << "\n";
     return 0;
 }
